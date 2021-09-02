0de6ea4e5131718484eb86234a211303abb1568c
Harinath kolli <hkolli@ftdi.com>
2021-09-02 13:23:24 +0530

    dummy rest api get endpoint

--- pages/api/list/[id].js
+++ pages/api/list/[id].js
@@ -1,13 +1,31 @@
-import listData from '../../mock-data/list.json'
+import listItem from '../../mock-data/list-by-id.json'
+
 
 export default function personHandler({ query: { id } }, res) {
-    console.log("page by id "+id);
-  const filtered = listData && listData.content && listData.content.filter((p) => p.batchNumber === id)
+  console.log("page by id " + id);
 
-  // list with id exists
-  if (filtered.length > 0) {
-    res.status(200).json(filtered[0])
+
+  if (id != 404) {
+
+    if (listItem) {
+      listItem.id = id;
+    }
+
+    res.status(200).json(listItem);
+
   } else {
+
     res.status(404).json({ message: `Item with id: ${id} not found.` })
   }
+
+
+  // const filtered = listData && listData.content && listData.content.filter((p) => p.batchNumber === id)
+
+  // // list with id exists
+  // if (filtered.length > 0) {
+  //   res.status(200).json(filtered[0])
+  // } else {
+  //   res.status(404).json({ message: `Item with id: ${id} not found.` })
+  // }
+
 }
